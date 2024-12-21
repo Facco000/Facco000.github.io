ca786b49354a3440c4252ce631beaebf245bf19b
Facco000 <facchinetti130@gmail.com>
2024-12-21 21:16:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     snowContainer.classList.add('snowflakes');
     document.body.appendChild(snowContainer);
 
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 30; i++) {
         setTimeout(() => {
             const snowflake = document.createElement('div');
             snowflake.classList.add('snowflake');
